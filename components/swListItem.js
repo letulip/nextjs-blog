0909120d6bedc520abff4e35dbdb5d2bab6cffef
Igor Vladimirskiy <letulip@Igors-Mac-mini.local>
2023-03-13 11:42:44 +0300

    fix: swListItem link generetion bug, add react course proceed md

--- components/swListItem.js
+++ components/swListItem.js
@@ -2,9 +2,10 @@ import Link from 'next/link'
 import utilStyles from '../styles/utils.module.css'
 
 export default function SWListItem(props) {
+  const type = props.type === `people` ? `people` : `planets`
   return (
-    <li className={utilStyles.listItemBig} key={props.idx + 1}>
-      <Link href={`/starwars/${props.type}/${props.idx + 1}`}>
+    <li className={utilStyles.listItem} key={props.idx + 1}>
+      <Link href={`/starwars/${type}/${props.idx + 1}`}>
         <h3 className={utilStyles.heading3after}>{props.item.name}</h3>
       </Link>
     </li>
---
+++ posts/react-course-proceed.md
@@ -0,0 +1,10 @@
+---
+title: 'React deep and intersting tutorial fully unlocked'
+date: '2023-03-12'
+---
+
+I was approved to get the full paid version of [Learn React Tutorial](https://react-tutorial.app/) and it's genuinely wonderful news!
+
+This course has a fantastic PWA Quiz for a smartphone so one can practice theory during an off-computer time and it's very thoughtful and convenient.
+
+BTW I fixed a bug in Starwars API implementation. The correct version can be viewed [here](https://letulip-nextjs-blog.vercel.app/starwars/)
